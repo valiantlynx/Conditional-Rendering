eeba7fcc792581c7f7d3b4bfc8d21ec925074150
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2022-06-09 03:42:54 +0200

    mmmh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = true;
+
+var currentTime = new Date(10010, 11, 1, 13).getHours();
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {
+        /* {isLoggedIn ? <h1>Hello</h1> : <Login />} */
+        currentTime > 12 && <h1>Why are u still working?</h1> //the and wont bother to run the rest of the code if the first part is false.
+      }
     </div>
   );
 }
